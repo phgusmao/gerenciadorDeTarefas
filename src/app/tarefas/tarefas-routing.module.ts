924379d2f54be097caf5b6010eb3626e4f46cc3a
Pedro Gusmao <pedro.gusmao1998@hotmail.com>
2021-07-16 14:06:52 -0300

    Projeto de tarefas

---
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -0,0 +1,32 @@
+import { Routes } from "@angular/router";
+
+import { ListarTarefaComponent } from "./listar";
+
+import { TarefaService } from "./shared";
+
+import { TarefasModule } from "./tarefas.module";
+
+import { CadastrarTarefaComponent } from "./cadastrar";
+
+export const TarefaRoutes: Routes = [
+    {
+        path: 'tarefas',
+        redirectTo: 'tarefas/listar'
+    },
+    {
+        path: 'tarefas/listar',
+        component: ListarTarefaComponent
+    },
+    {
+        path: 'tarefas/shared',
+        component: TarefaService
+    },
+    {
+        path: 'tarefas',
+        component: TarefasModule
+    },
+    {
+        path: 'tarefas/cadastrar',
+        component: CadastrarTarefaComponent
+    }
+]
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar';
+
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CadastrarTarefaComponent } from './cadastrar';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarTarefaComponent,
+    CadastrarTarefaComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers: [
+    TarefaService
   ]
 })
 export class TarefasModule { }
